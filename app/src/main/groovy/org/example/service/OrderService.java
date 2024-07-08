347fbc6c558a1b5da7246c78a09aa854caa78ffb
prajwalp3 <prajwalprabhu025@gmail.com>
2024-07-08 23:18:04 +0530

    spring boot essentials

---
+++ app/src/main/groovy/org/example/service/OrderService.java
@@ -0,0 +1,15 @@
+package org.example.service;
+
+import org.example.bean.UserConfig;
+
+public class OrderService
+
+{
+
+
+    public static OrderService createInstance(UserService userService, UserConfig userConfig){
+
+        return null;
+
+    }
+}
